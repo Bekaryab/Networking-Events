dcb9f71d7658a864bf95f7397121eb06d1e80391
Bekarys <abievb91@gmail.com>
2026-02-11 22:39:58 +0500

    Initial Commit

---
+++ networking&Events/networking_EventsApp.swift
@@ -0,0 +1,17 @@
+//
+//  networking_EventsApp.swift
+//  networking&Events
+//
+//  Created by bekarys abiev on 11.02.2026.
+//
+
+import SwiftUI
+
+@main
+struct networking_EventsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ networking&EventsTests/networking_EventsTests.swift
@@ -0,0 +1,17 @@
+//
+//  networking_EventsTests.swift
+//  networking&EventsTests
+//
+//  Created by bekarys abiev on 11.02.2026.
+//
+
+import Testing
+@testable import networking_Events
+
+struct networking_EventsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
